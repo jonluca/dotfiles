353986cc02ffad4968bfb1b45df9b0d8fbc5e175
JonLuca DeCaro <jdecaro@usc.edu>
2019-01-11 06:16:17 -0800

    Modified dot files, added options and functions

--- install.sh
+++ install.sh
@@ -6,24 +6,35 @@ ln -s orig/.zshrc ~
 ln -s orig/.sandboxrc ~
 ln -s orig/.vimrc ~
 
+# Install sensible vim options
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/sensible.git
+vim -u NONE -c "helptags sensible/doc" -c q
+
 # Install brew 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
 
 # Set up shell
 brew install zsh
 chsh -s $(which zsh)
 
-# set up theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 
-mkdir /tmp/rand123999
-cd /tmp/rand123999
+cd mytmpdir
 git clone git@github.com:powerline/fonts.git
 cd fonts
 chmod +x ./install.sh
 ./install.sh
 cd orig
+rm -rf mytmpdir
+
 # Set up mac config
 ./.macos
 
-echo "Done! Remember to setup visudo"
+# Set up git settings
+./.settings
+
+echo "Done! Remember to setup visudo!"
